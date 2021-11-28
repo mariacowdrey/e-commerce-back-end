ea6b12ab45e2fb2a97858ca4b81efc7b772f2e0d
Maria Cowdrey <mariacowdrey@yahoo.com>
2021-11-28 14:17:35 -0800

    fixed issue with sequelize sync

--- server.js
+++ server.js
@@ -12,11 +12,8 @@ app.use(express.urlencoded({ extended: true }));
 app.use(routes);
 
 // sync sequelize models to the database, then turn on the server
-// app.listen(PORT, () => {
-//   console.log(`App listening on port ${PORT}!`);
-// });
-
-// turn on connection to db and server
-sequelize.sync({ force: true }).then(() => {
-  app.listen(PORT, () => console.log('Now listening'));
+sequelize.sync({ force: false }).then(() => {
+app.listen(PORT, () => {
+  console.log(`App listening on port ${PORT}!`);
+});
 });
